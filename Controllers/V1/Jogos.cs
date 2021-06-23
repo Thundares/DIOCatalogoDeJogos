a66e742dc2c8493396a84ec759abbd479057c82c
Kenzo Mippo <pedrac.u@gmail.com>
2021-06-23 18:35:31 -0300

    Update Controller to use the Interface

--- Controllers/V1/Jogos.cs
+++ Controllers/V1/Jogos.cs
@@ -1,9 +1,11 @@
 ﻿using CatalogoDeJogos.InputModel;
+using CatalogoDeJogos.Services;
 using CatalogoDeJogos.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,44 +15,97 @@ namespace CatalogoDeJogos.Controllers.V1
     [ApiController]
     public class Jogos : ControllerBase
     {
+
+        private readonly IJogoService _jogoService;
+
+        public Jogos(IJogoService IJS) 
+        {
+            _jogoService = IJS;
+        }
+
         //visualizar--------------
         [HttpGet]
-        public async Task<ActionResult<List<JogoViewModel>>> Obter()
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
-            return Ok(); 
+            var result = await _jogoService.Obter(pagina ,quantidade);
+
+            if (result.Count() == 0) 
+            {
+                return NoContent();
+            }
+            return Ok(result); 
         }
 
         [HttpGet("{idJogo:guid}")]
-        public async Task<ActionResult<List<JogoViewModel>>> Obter(Guid idJogo)
+        public async Task<ActionResult<List<JogoViewModel>>> Obter([FromRoute]Guid idJogo)
         {
-            return Ok();
+            var result = await _jogoService.Obter(idJogo);
+
+            if (result == null) 
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
         }
 
         //inserir-----------------
         [HttpPost]
-        public async Task<ActionResult<JogoViewModel>> InserirJogo(JogoInputModel jogo)
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody]JogoInputModel jogo)
         {
-            return Ok();
+            try
+            {
+                var result = await _jogoService.Inserir(jogo);
+                return Ok(result);
+            }
+            catch (JogoCadastradoException ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
         }
 
         //update------------------
         [HttpPut("{idJogo:guid}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogo)
+        public async Task<ActionResult> AtualizarJogo([FromRoute]Guid idJogo, [FromBody]JogoInputModel jogo)
         {
-            return Ok();
+            try
+            {
+                await _jogoService.Atualizar(idJogo, jogo);
+                return Ok();    
+            }
+            catch (JogoNaoCadastradoException ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
 
         [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        public async Task<ActionResult> AtualizarJogo([FromRoute]Guid idJogo, [FromRoute]double preco)
         {
-            return Ok();
+            try
+            {
+                await _jogoService.Atualizar(idJogo, preco);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound("Jogo não encontrado");
+            }
         }
 
         //deletar----------------
         [HttpDelete("{idJogo:guid}")]
-        public async Task<ActionResult> DeletarJogo(Guid idJogo) 
+        public async Task<ActionResult> DeletarJogo([FromRoute]Guid idJogo) 
         {
-            return Ok();
+            try
+            {
+                await _jogoService.Remover(idJogo);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound("Jogo não encontrado");
+            }
         }
     }
 }
