0c2c04cfe7ac91d7a1b65ec4cc0ba885ba0d7dce
Kenzo Mippo <pedrac.u@gmail.com>
2021-06-23 17:33:33 -0300

    Builded a controller's template without any implementation

---
+++ Controllers/V1/Jogos.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoDeJogos.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Jogos : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        // inserir
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo()
+        {
+            return Ok();
+        }
+
+        //update
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> DeletarJogo(Guid idJogo) 
+        {
+            return Ok();
+        }
+    }
+}
