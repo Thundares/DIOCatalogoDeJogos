ffc468683982f0451eec6e1042b195cd05b6dd36
Kenzo Mippo <pedrac.u@gmail.com>
2021-06-25 19:34:57 -0300

    Added JogosRepository. Working on a in memory database

---
+++ Repositories/JogoRepository.cs
@@ -0,0 +1,57 @@
+﻿using CatalogoDeJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoDeJogos.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+
+        private static Dictionary<Guid, Jogo> jogos = new Dictionary<Guid, Jogo>() { };
+
+        public Task Atualizar(Jogo jogo)
+        {
+            jogos.[jogo.id] = jogo;
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            //close db conection when implemented
+        }
+
+        public Task Inserir(Jogo jogo)
+        {
+            jogos.Add(jogo.id, jogo);
+            return Task.CompletedTask;
+        }
+
+        public Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            return Task.FromResult(jogos.Values.Skip((pagina - 1) * quantidade).Take(quantidade).ToList());
+        }
+
+        public Task<Jogo> Obter(Guid id)
+        {
+            if (!jogos.ContainsKey(id)) 
+            {
+                return null;
+            }
+
+            return Task.FromResult(jogos[id]);
+        }
+
+        public Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            return Task.FromResult(jogos.Values.Where(jogo => jogo.nome.Equals(nome) && jogo.produtora.Equals(produtora)).ToList());
+        }
+
+        public Task Remover(Guid id)
+        {
+            jogos.Remove(id);
+            return Task.CompletedTask;
+        }
+    }
+}
